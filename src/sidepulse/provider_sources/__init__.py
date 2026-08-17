4cca57928f00b62d210d5069433554aab9cdf7a9
Jonathan R. Reed <130617826+JonathanRReed@users.noreply.github.com>
2026-08-16 19:43:49 -0500

    feat: add first-party provider source package

---
+++ src/sidepulse/provider_sources/__init__.py
@@ -0,0 +1,5 @@
+"""First-party SidePulse provider usage sources."""
+
+from .runtime import collect_provider_usage, collect_provider_usage_all
+
+__all__ = ["collect_provider_usage", "collect_provider_usage_all"]
